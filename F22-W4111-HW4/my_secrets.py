e539d12b4e8e260047e817c4de17c2944f28c380
William Das <williamhdas@gmail.com>
2022-12-26 19:03:53 -0500

    Initial commit

---
+++ F22-W4111-HW4/my_secrets.py
@@ -0,0 +1,4 @@
+aura_url = "neo4j+s://25653554.databases.neo4j.io"
+aura_user = "neo4j"
+aura_pw = "mPQJhDh6Yrauk-Y9AF9yiNRvlQ-X3ciRMUES4jd0-5c"
+mongodb_url = "mongodb+srv://wdas:Hn6X3n5VrHdRB8e@cluster0.kkpls2i.mongodb.net/test"
---
+++ README.md
@@ -0,0 +1,2 @@
+# COMS 4111: Intro to Databases
+Homeworks for COMS 4111: Intro to Databases (Fall 2022).
